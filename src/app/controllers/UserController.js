0631a03b19bf2fb84f216f8545ff61ea6c668679
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-26 18:51:48 -0300

    Add post User

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,21 @@
+import User from '../models/Users';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User Exists' });
+    }
+
+    const { id, name, email, speaker } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      speaker,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import Router from 'express';
-import User from './app/models/Users';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Moises',
-    email: 'moises@gmail.com',
-    password_hash: '12345',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;

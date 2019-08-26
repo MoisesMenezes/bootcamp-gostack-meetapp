c0337c3e02145794b81cda33453337503ee19221
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-26 18:38:54 -0300

    Add Model user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import Router from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Moisés' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Moises',
+    email: 'moises@gmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

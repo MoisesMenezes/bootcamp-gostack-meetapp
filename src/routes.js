86d4e420c9948b657feb77ccf8814496e4134384
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-26 15:34:27 -0300

    Add config prettier and Eslint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import Router from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Moisés' });
 });
 
-module.exports = routes;
+export default routes;

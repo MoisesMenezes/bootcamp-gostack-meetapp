80e78ab5b84bcb842cf3841f6f01dfd5de846775
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-27 01:40:10 -0300

    Add update password

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,34 @@ class UserController {
       speaker,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const emailExists = await User.findOne({
+        where: { email },
+      });
+
+      if (emailExists) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, speaker } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      speaker,
+    });
+  }
 }
 
 export default new UserController();
